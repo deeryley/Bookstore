f173245992a8f802a7226925d0c22dfa4699e51e
Vasilena <vt.merica@gmail.com>
2022-04-22 16:38:31 +0300

    Connect to the Web Service

---
+++ Bookstore.Web/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Bookstore.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        private BookCatalogService _bookCatalog;
+
+        public BooksController(BookCatalogService bookCatalog)
+        {
+            _bookCatalog = bookCatalog;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookCatalog.GetAllBooks();
+
+            return View(books);
+        }
+    }
+}
--- Bookstore.Web/Startup.cs
+++ Bookstore.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Bookstore.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,13 @@ namespace Bookstore.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<BookCatalogService>(options =>
+           {
+               options.BaseAddress = new Uri("https://localhost:44355/api/");
+           });
+
+
+
             services.AddControllersWithViews();
         }
 
